12a6530ab16fa2a4de5735933c49e2dfea6423bb
Tyler Fletcher <fletchertyler914@yahoo.com>
2019-06-12 18:26:13 -0500

    Code Cleanup, Flow Error Messages

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,35 +1,23 @@
 import { Injectable } from '@angular/core';
-
-import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { auth } from 'firebase/app';
-
 import { LoginResponse, User } from '../models/login-response.model';
-import { Observable, BehaviorSubject, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { LoginModal } from '../login-modal/login-modal.component';
-import * as firebase from 'firebase';
 import { ContactForm } from '../models/contact-form.model';
-import { AuthService } from './auth.service';
-import { filter, map, tap, first } from 'rxjs/operators';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
   users: Observable<LoginResponse[]>;
-  fireAuth = firebase.auth();
   public currentUser$: Observable<firebase.User> = this.afAuth.user;
 
-  constructor(private db: AngularFirestore, private afAuth: AngularFireAuth) { }
-
-  createZingleUser(user: LoginResponse) {
-    const data = JSON.parse(JSON.stringify(user));
-    return this.db.collection('users').add(data);
-  }
+  constructor(private db: AngularFirestore, private afAuth: AngularFireAuth) {}
 
   login(user: LoginModal) {
     return this.afAuth.auth
-      .setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+      .setPersistence('local')
       .then(() => {
         return this.afAuth.auth.signInWithEmailAndPassword(
           user.userName,
@@ -40,7 +28,7 @@ export class FirebaseService {
 
   signUp(user: LoginModal) {
     return this.afAuth.auth
-      .setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+      .setPersistence('local')
       .then(() => {
         return this.afAuth.auth.createUserWithEmailAndPassword(
           user.userName,
@@ -50,11 +38,18 @@ export class FirebaseService {
   }
 
   uploadContact(contact: ContactForm) {
-    if (!!this.fireAuth.currentUser) {
-      const data = JSON.parse(JSON.stringify(contact));
-      return this.db.collection('users').doc(this.fireAuth.currentUser.uid).collection('contacts').add(data);
+    const data = JSON.parse(JSON.stringify(contact));
+
+    // If User Not Logged In, Create Contact In Geneeral Contact Collection
+    if (!!this.afAuth.auth.currentUser) {
+      return this.db
+        .collection('users')
+        .doc(this.afAuth.auth.currentUser.uid)
+        .collection('contacts')
+        .add(data);
     } else {
-      return Promise.reject(new Error('You Are Not Logged In!'))
+      return this.db.collection('contacts').add(data);
+      // return Promise.reject(new Error('You Are Not Logged In!'))
     }
   }
 
@@ -62,20 +57,20 @@ export class FirebaseService {
     return this.afAuth.auth.signOut();
   }
 
-    // Sets user data to firestore after succesful login
-    updateUserData(user: User) {
-      const userRef: AngularFirestoreDocument<User> = this.db.doc(
-        `users/${user.uid}`
-      );
+  // Sets user data to firestore after succesful login
+  updateUserData(user: User) {
+    const userRef: AngularFirestoreDocument<User> = this.db.doc(
+      `users/${user.uid}`
+    );
 
-      const data: User = {
-        uid: user.uid,
-        email: user.email || null,
-        displayName: user.displayName || user.email,
-        photoURL: user.photoURL || 'https://goo.gl/Fz9nrQ',
-        token: user.token
-      };
+    const data: User = {
+      uid: user.uid,
+      email: user.email || null,
+      displayName: user.displayName || user.email,
+      photoURL: user.photoURL || 'https://goo.gl/Fz9nrQ',
+      token: user.token
+    };
 
-      return userRef.set(data, {merge: true});
-    }
+    return userRef.set(data, { merge: true });
+  }
 }
